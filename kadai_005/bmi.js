3ed67033a2adbdfb866550f9bdc7be6eac4947bc
2drisa <risa228723@gmail.com>
2024-08-30 20:03:31 +0900

    bmi.jsを編集

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,7 @@
 // 変数の宣言・値の代入
 let userWeight = 68 ;
 let userHeight = 1.7 ;
-let userBmi = (68 / (1.7 * 1.7));
+let userBmi = (userWeight / (userHeight * userHeight));
 
 // 四則演算
 console.log(userBmi);
