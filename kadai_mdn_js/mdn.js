e7c14dfecf7a0244026a46cf30257e029066a8f0
2drisa <risa228723@gmail.com>
2024-09-19 20:51:06 +0900

    kadai_mdnを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,5 @@
+const moonLanding = new Date('September 22, 23');
+const birthday = new Date('September 22, 23');
+const date1 = birthday.getDate();
+
+console.log(moonLanding.getFullYear() + '年' + moonLanding.getMonth() + '月' + date1 + '日');
