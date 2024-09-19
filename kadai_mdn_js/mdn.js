9337a03abadb5a6e06c51067f5d551182615159c
2drisa <risa228723@gmail.com>
2024-09-19 21:03:00 +0900

    mdn.jsを編集

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,3 @@
 const moonLanding = new Date('September 22, 23');
-const birthday = new Date('September 22, 23');
-const date1 = birthday.getDate();
 
-console.log(moonLanding.getFullYear() + '年' + moonLanding.getMonth() + '月' + date1 + '日');
+console.log(moonLanding.getFullYear() + '年' + moonLanding.getMonth() + '月' + moonLanding.getDate() + '日');
